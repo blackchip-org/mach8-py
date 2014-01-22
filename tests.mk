93ce284af569cb01695e67898936836ccfbd5ff2
Mike McGann <mike.mcgann@nasa.gov>
2014-01-22 15:06:00 -0500

    Updated testing makefile without absolute path to nose

--- tests.mk
+++ tests.mk
@@ -1,6 +1,6 @@
 
 all:
-	-PYTHONPATH=source /usr/local/bin/nosetests --with-coverage \
+	-PYTHONPATH=source nosetests --with-coverage \
 		--cover-package=mach8 \
 		--cover-html --cover-html-dir=coverage --with-xunit \
 		--xunit-file=nosetests.xml
